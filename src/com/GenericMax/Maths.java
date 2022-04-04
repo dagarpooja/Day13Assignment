baedbd55c0478c3c2d119f0fa8944e03bdefe00c
dagarpooja <pdagar67@gmail.com>
2022-04-04 15:16:19 +0530

    generic solution

---
+++ src/com/GenericMax/Maths.java
@@ -0,0 +1,19 @@
+package com.GenericMax;
+
+public class Maths {
+
+
+		public <T extends Comparable<T>> T findMax(T a, T b, T c) {
+			System.out.println("The numbers are : " + a + ","
+					+ b + "," + c);
+			T max = a;
+			if (max.compareTo(b) < 0) {
+				max = b;
+			}
+			if (max.compareTo(c) < 0) {
+				max = c;
+			}
+			return max;
+		}
+
+}
---
+++ src/com/GenericMax/MaximumTest.java
@@ -0,0 +1,36 @@
+package com.GenericMax;
+
+import org.junit.jupiter.api.Test;
+
+class MaximumTest {
+
+	@Test
+	public void findIntMax() {
+		Maths m = new Maths();
+		
+		int a = (int) (Math.random() * 1000);
+		Integer b = (int) (Math.random() * 1000);
+		Integer c = (int) (Math.random() * 1000);
+		System.out.println(m.findMax(a, b, c));
+		System.out.println("-----------------------------");
+
+		float x = (float) (Math.random() * 1000);
+		float y = (float) (Math.random() * 1000);
+		float z = (float) (Math.random() * 1000);
+		System.out.println(m.findMax(x, y, z));
+		System.out.println("-----------------------------");
+
+		String s1 = "hello";
+		String s2 = "How";
+		String s3 = "are you?";
+		System.out.println(m.findMax(s1, s2, s3));
+		System.out.println("-----------------------------");
+
+		Employee e1 = new Employee("a", 10000);
+		Employee e2 = new Employee("b", 20000);
+		Employee e3 = new Employee("c", 30000);
+		System.out.println(m.findMax(e1, e2, e3));
+	}
+	}
+
+
